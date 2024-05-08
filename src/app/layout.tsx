25c6b10f06330808f7cc1909058859c48f32ee71
Remco <57683378+remcostoeten@users.noreply.github.com>
2024-05-08 03:23:24 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 import "./globals.css";
 import React from "react";
 
@@ -24,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.className}>
       <body>{children}</body>
+      <SpeedInsights/>
       <Analytics />
     </html>
   );
